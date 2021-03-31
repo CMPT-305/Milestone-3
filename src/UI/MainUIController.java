b9c650506e6a8fefb0cf9df441a509ddee302058
ripclaw52 <ryleygoodine96@gmail.com>
2021-03-31 10:30:33 -0600

    Merge upstream/main into main
    Added stats text area on the left side showing average values

--- src/UI/MainUIController.java
+++ src/UI/MainUIController.java
@@ -10,6 +10,7 @@ import Data.FindAccount;
 import Data.Searcher;
 import Data.Statistics;
 import static Data.Statistics.mean;
+import java.awt.Font;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -76,13 +77,13 @@ public class MainUIController implements Initializable {
     
     // tab 2
     @FXML private ChoiceBox<String> selectWard;
+    @FXML private TextArea wardGraphic;
     
     public List<Data> masterData = new ArrayList<>(newSearcher.getAllAccounts());
     public Map<Integer, Data> map = new HashMap<>(newSearcher.getAllAccountsM());
     public Map<String, Map<String, List<Double>>> graphicMap = new TreeMap<>(newSearcher.getSortedMapByWard());
     public ObservableList<Data> listData = FXCollections.observableArrayList(masterData);
     
-    final static String performance = "Performance:";
     /**
      * initialize - initializes the tableAssessment for FXML
      * create
@@ -205,16 +206,25 @@ public class MainUIController implements Initializable {
     @FXML
     void graphicSearch(ActionEvent event) {
         XYChart.Series <String, Number> series = new XYChart.Series();
+        StringBuilder stringBuilder = new StringBuilder();
         
         String tempWard = selectWard.getValue();
+        stringBuilder.append(tempWard+"\n"+"-------------------------------\n");
+        
         Set<String> wardSet = new TreeSet<>(graphicMap.get(tempWard).keySet());
         for (String str: wardSet) {
-            Double average = mean(graphicMap.get(tempWard).get(str));
-            series.getData().add(new XYChart.Data(str, average));
+            if (str.equals("")) {
+                // skips over empty fields
+            } else {
+                Double average = mean(graphicMap.get(tempWard).get(str));
+                series.getData().add(new XYChart.Data(str, average));
+                stringBuilder.append("\t" + str + ":  $" + String.format("%,.2f", average) + "\n\n");
+            }
         }
         series.setName(tempWard);
         graphName.setText(tempWard);
         barChart.getData().setAll(series);
+        wardGraphic.setText(stringBuilder.toString());
     }
     
     /**
@@ -226,6 +236,7 @@ public class MainUIController implements Initializable {
         selectWard.setValue("");
         graphName.setText("");
         barChart.getData().setAll();
+        wardGraphic.setText("");
     }
     /**
      * showStats - takes a data list and prints out the statistics.
